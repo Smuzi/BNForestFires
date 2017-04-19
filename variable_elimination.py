d19dc872a02039297fa7d537ee6ffa4244619475
smuzi <uzi.smadja@gmail.com>
2017-04-19 13:43:06 +0300

    cpt_maker and discrete dataset

---
+++ variable_elimination.py
@@ -0,0 +1,7 @@
+# node_list: [node1,node2..], parents_list [[node1_parent1,node1_parent2..],[..],..]
+# if a node has not parents, empty list [].
+# variable elimination order:
+def variable_elimination(data,node_list,parents_list)
+
+if __name__ == '__main__':
+    create_cpt('RH', 5, ['day'], [7]) #needs further checking with more than 1 parent
